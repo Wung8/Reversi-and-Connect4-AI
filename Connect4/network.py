a42d5a7c76f5d7b81490332efcd6e30da671cab4
Wung8 <115668186+Wung8@users.noreply.github.com>
2024-09-19 14:51:15 -0400

    Add files via upload

--- Connect4/network.py
+++ Connect4/network.py
@@ -9,23 +9,17 @@ class Network(nn.Module):
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=27, kernel_size=3, stride=1),
             nn.ReLU())
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=27, out_channels=27, kernel_size=3, stride=1),
-            nn.ReLU())
         
-        self.fc1 = nn.Linear(6*6*27+4*4*27+8*8*3,100)
+        self.fc1 = nn.Linear(4*5*27+6*7*3,100)
         self.act = nn.ReLU()
         self.fc2 = nn.Linear(100,1)
 
     def forward(self,x):
         x1 = self.layer1(x)
-        x2 = self.layer2(torch.squeeze(x1))
 
         x1 = torch.flatten(x1)
-        x2 = torch.flatten(x2)
 
-        x = torch.cat((torch.flatten(x),x1,x2))
+        x = torch.cat((torch.flatten(x),x1))
 
         x = self.fc1(x)
         x = self.act(x)
